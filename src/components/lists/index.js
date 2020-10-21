5d571ff72dccafd7c7d2c1a6c1d1f136d6634b83
Husen Efendi <husenef@gmail.com>
2020-10-21 09:07:53 +0700

    update

--- src/components/lists/index.js
+++ src/components/lists/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Icon, InlineIcon } from "@iconify/react";
+import { Icon } from "@iconify/react";
 import crossIcon from "@iconify/icons-entypo/cross";
 
 const Item = () => {
